7939e1bcec9bb2ba6c079b8bab907dd239312514
blawness <razzorite@gmail.com>
2026-05-04 12:59:37 +0700

    feat: add profileImage uploadthing route

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -16,6 +16,17 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ file }) => {
       return { url: file.ufsUrl }
     }),
+  profileImage: f({
+    image: { maxFileSize: "2MB", maxFileCount: 1 },
+  })
+    .middleware(async () => {
+      const session = await getServerSession(authOptions)
+      if (!session) throw new Error("Unauthorized")
+      return { userId: session.user.id }
+    })
+    .onUploadComplete(async ({ file }) => {
+      return { url: file.ufsUrl }
+    }),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
