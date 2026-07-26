823d909f1c2b43aa6ecadd5fbb5688952b6e8438
blawness <razzorite@gmail.com>
2026-07-26 19:46:27 +0700

    test: add integration tests for /api/properties GET/POST
    Adds polyfills in jest.setup.js for Web APIs (Request, Response,
    TextEncoder, ReadableStream, etc.) required by Next.js 16 in jsdom.
    
    Adds undici as devDependency for the fetch/Request polyfill.

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,33 @@
 import '@testing-library/jest-dom'
+
+const basicGlobals = {
+  TextEncoder: require('util').TextEncoder,
+  TextDecoder: require('util').TextDecoder,
+  ReadableStream: require('stream/web').ReadableStream,
+  WritableStream: require('stream/web').WritableStream,
+  TransformStream: require('stream/web').TransformStream,
+  MessagePort: require('worker_threads').MessagePort,
+  MessageChannel: require('worker_threads').MessageChannel,
+  BroadcastChannel: require('worker_threads').BroadcastChannel,
+  Blob: require('buffer').Blob,
+  File: require('buffer').File,
+}
+for (const [key, value] of Object.entries(basicGlobals)) {
+  if (typeof globalThis[key] === 'undefined') {
+    globalThis[key] = value
+  }
+}
+
+const undici = require('undici')
+const undiciGlobals = {
+  FormData: undici.FormData,
+  Headers: undici.Headers,
+  Request: undici.Request,
+  Response: undici.Response,
+  fetch: undici.fetch,
+}
+for (const [key, value] of Object.entries(undiciGlobals)) {
+  if (typeof globalThis[key] === 'undefined') {
+    globalThis[key] = value
+  }
+}
