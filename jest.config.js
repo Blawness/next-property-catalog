b9267a0a1d75ac50a80c1b4d99579a793f7a1fb1
blawness <razzorite@gmail.com>
2026-07-26 19:44:01 +0700

    test: configure jest coverage collection (lib/api/hooks/components)

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,13 @@ const config = {
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverageFrom: [
+    'lib/**/*.{ts,tsx}',
+    'app/api/**/*.{ts,tsx}',
+    'hooks/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
